2a9041c2a1222289f144914412e0bbfbb0b5239b
jzwill <zwillijt@miamioh.edu>
2026-09-17 08:58:43 -0400

    home screen

--- script.js
+++ script.js
@@ -126,3 +126,47 @@ function showDivsPhotos(n) {
   }
   x[slideIndex - 1].style.display = "block";
 }
+
+var slideIndex = 1;
+showDivsGames(slideIndex);
+
+function plusDivsGames(n) {
+  showDivsGames((slideIndex += n));
+}
+
+function showDivsGames(n) {
+  var i;
+  var x = document.getElementsByClassName("gameSlides");
+  if (n > x.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = x.length;
+  }
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex - 1].style.display = "block";
+}
+
+var slideIndex = 1;
+showDivsGraphic(slideIndex);
+
+function plusDivsGraphic(n) {
+  showDivsGraphic((slideIndex += n));
+}
+
+function showDivsGraphic(n) {
+  var i;
+  var x = document.getElementsByClassName("graphicSlides");
+  if (n > x.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = x.length;
+  }
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex - 1].style.display = "block";
+}
